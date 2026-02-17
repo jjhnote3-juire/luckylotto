6e53dc286fd547fe25a619fb6f19d780f3054eab
jjhq80416 <jjhq80416@gmail.com>
2026-02-17 08:54:59 +0000

    feat: 초기 프로젝트 파일 업로드

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-// Add JS here
+const generateBtn = document.querySelector('.generate-btn');
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+
+const getNumberColor = (number) => {
+    if (number <= 10) return '#fbc400'; // 노란색
+    if (number <= 20) return '#69c8f2'; // 파란색
+    if (number <= 30) return '#ff7272'; // 빨간색
+    if (number <= 40) return '#aaa'; // 회색
+    return '#b0d840'; // 초록색
+};
+
+const generateLottoNumbers = () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    sortedNumbers.forEach(number => {
+        const lottoBall = document.createElement('div');
+        lottoBall.classList.add('lotto-ball');
+        lottoBall.textContent = number;
+        lottoBall.style.backgroundColor = getNumberColor(number);
+        lottoNumbersContainer.appendChild(lottoBall);
+    });
+};
+
+generateBtn.addEventListener('click', generateLottoNumbers);
+
+// Initial generation
+generateLottoNumbers();
